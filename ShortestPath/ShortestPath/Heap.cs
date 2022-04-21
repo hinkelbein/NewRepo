d43e624761f89662bd6c6f89c59e9dcdd7750de7
JavadEs12 <82588563+JavadEs12@users.noreply.github.com>
2022-04-21 17:41:33 +0200

    nothing

---
+++ ShortestPath/ShortestPath/Heap.cs
@@ -0,0 +1,149 @@
+﻿namespace ShortestPath
+{
+    public class Heap
+    {
+        public Node root;
+        public Node pointer;
+        public int count;
+
+        public Heap(Dictionary<string, Node> nodes)
+        {
+            count = 0;
+            foreach (KeyValuePair<string, Node> node in nodes)
+            {
+                Add(node.Value);
+            }
+        }
+
+        private void Add(Node node)
+        {
+            if (root == null)
+            {
+                root = node;
+                count++;
+            }
+            else
+            {
+
+                pointer = root;
+                string bitcount = Convert.ToString(count + 1, 2);  //converts integer value to its binary form 
+                for (int i = 1; i < bitcount.Length; i++)   //start from second element of binary to exclude the parent
+                {
+                    if (bitcount[i] == '0')
+                    {
+                        if (pointer.Left == null)
+                        {
+                            pointer.Left = new Node(pointer); //Node(Node P) is a costructor which creates an empty Node with its Parent
+                        }
+                        pointer = pointer.Left;
+                    }
+                    else
+                    {
+                        if (pointer.Right == null)
+                        {
+                            pointer.Right = new Node(pointer);
+                        }
+                        pointer = pointer.Right;
+                    }
+                }
+                pointer.Cost = node.Cost;
+
+                while (true)
+                {
+                    if (pointer == root) { break; }
+                    if (pointer.Cost < pointer.Parent.Cost)
+                    { //Switch data
+                        double tempCost = pointer.Cost;
+                        pointer.Cost = pointer.Parent.Cost;
+                        pointer.Parent.Cost = tempCost;
+                        pointer = pointer.Parent;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+                count++;
+            }
+        }
+        public double Remove()
+        {
+            double output = root.Cost;
+            pointer = root;
+            string bitcount = Convert.ToString(count, 2);
+            for (int i = 1; i < bitcount.Length; i++)
+            {
+                if (bitcount[i] == '0')
+                {
+                    pointer = pointer.Left;
+
+                }
+                else
+                {
+                    pointer = pointer.Right;
+                }
+            }
+            root.Cost = pointer.Cost;  // set root equal to last filled space in heap
+            try
+            {
+                //delete last filled space in heap 
+                if (pointer.Parent.Left == pointer)
+                {
+                    pointer.Parent.Left = null;
+                }
+                else
+                {
+                    pointer.Parent.Right = null;
+                }
+                count--;
+                Heapify();  // perculate down new root
+            }
+            catch
+            {
+                root = null;
+            }
+            return output;
+        }
+
+
+        private void Heapify()
+        {
+            Node compare;
+            pointer = root;
+
+            while (true)
+            {
+                if (pointer.Left == null)
+                {
+                    break;
+                }
+                if (pointer.Right == null)
+                {
+                    compare = pointer.Left;
+                }
+                else
+                {
+                    if (pointer.Left.Cost <= pointer.Right.Cost)
+                    {
+                        compare = pointer.Left;
+                    }
+                    else
+                    {
+                        compare = pointer.Right;
+                    }
+                }
+                if (pointer.Cost > compare.Cost)
+                {
+                    double tempCost = pointer.Cost;
+                    pointer.Cost = compare.Cost;
+                    compare.Cost = tempCost;
+                    pointer = compare;
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
--- ShortestPath/ShortestPath/Node.cs
+++ ShortestPath/ShortestPath/Node.cs
@@ -2,17 +2,41 @@
 {
     public class Node
     {
+        public Node Parent;
+        public Node Left;
+        public Node Right;
+
         public string ID { get; set; }
         public double Cost { get; set; }             // Node Cost.
         public string Successor { get; set; }        // Node Successor.
-        public static List<Node> NetworkNodes = new List<Node>( );     // Aggregation of all the network nodes as list of nodes.
+        public static List<Node> NetworkNodes = new List<Node>();     // Aggregation of all the network nodes as list of nodes.
 
-        public Node(string ID,double Cost,string Successor)
+        public Node(string ID, double Cost, string Successor)
         {
             this.ID = ID;
             this.Cost = Cost;
             this.Successor = Successor;
         }
 
+        public Node(double cost, Node p)
+        {
+            Cost = cost;
+            Parent = p;
+        }
+
+        public Node(Node p)
+        {
+            Parent = p;
+        }
+
+        public Node(double cost)
+        {
+            Cost = cost;
+        }
+
+        public Node()
+        {
+
+        }
     }
 }
